8b7858cd5b4f8999f06d0a114d2b83fa25a62219
MoacirMSJ <moacirmascarenha86@gmail.com>
2022-12-22 20:50:23 -0300

    Criada estrutura inicial para cursos

--- fga/models.py
+++ fga/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=200)
     age = models.IntegerField()
+
+class Course(models.Model):
+    title = models.CharField(max_length=25)
+    duration = models.IntegerField()
+    description = models.CharField(max_length=150)
+    credit_number = models.IntegerField()
+    icon = models.CharField(max_length=150)
--- fga/views.py
+++ fga/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Course
 
 def index(request):
     return render(request,'home/index.html' )
+
+def courses(request, id):
+    curso = Course.objects.get(id=id)
+    print(str(curso))
+    return render(request, 'courses/index.html', {"course": curso })
