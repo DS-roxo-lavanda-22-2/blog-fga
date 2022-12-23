d2f72395d579902a94095307f919aabdc8198860
aqela-batata-alt <victormoreiraalmeida@gmail.com>
2022-12-23 01:44:38 -0300

    buscar no banco e checar e pegar os valores do form

--- fga/forms.py
+++ fga/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class Login(forms.Form):
     username = forms.CharField(label='Username', max_length=100)
-    password = forms.CharField(label='Passoword', max_length=100)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
