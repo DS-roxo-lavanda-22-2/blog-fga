99410162e2815e2384ad8429dff5f0739f39c4ba
MoacirMSJ <moacirmascarenha86@gmail.com>
2022-12-22 21:08:34 -0300

    criado esboço view

--- fga/views.py
+++ fga/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import Login
 
 def index(request):
     return render(request,'home/index.html' )
+
+
+def login(request):
+    form = Login()
+    # pegar os valores do form que são o username e password
+    # buscar usuário no banco(model) com o username e checar se a senha é igual
+    return render(request,'login/index.html',{"form": form} )
